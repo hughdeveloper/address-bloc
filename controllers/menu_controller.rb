9e6253cb17d8e6dfd5cad52830a189c1d4cf1b6d
Hugh Bethell <hbethell@outlook.com>
2018-01-12 18:57:44 -0800

    checkpoint 6

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -40,7 +40,7 @@ class MenuController
         main_menu
       when 4
         system "clear"
-        read_csv
+        search_entries
         main_menu
       when 5
         system "clear"
@@ -108,9 +108,64 @@ class MenuController
     end
 
     def search_entries
+      print "Search by name: "
+      name = gets.chomp
+
+      match = address_book.binary_search(name)
+      system "clear"
+
+      if match
+        puts match.to_s
+        search_submenu(match)
+      else
+        puts "No match found for #{name}"
+      end
+    end
+
+    def delete_entry(entry)
+     address_book.entries.delete(entry)
+     puts "#{entry.name} has been deleted"
+    end
+
+    def edit_entry(entry)
+
+     print "Updated name: "
+     name = gets.chomp
+     print "Updated phone number: "
+     phone_number = gets.chomp
+     print "Updated email: "
+     email = gets.chomp
+     #use .empty? to set attributes on entry only if a valid attribute was read from the user imput
+     entry.name = name if !name.empty?
+     entry.phone_number = phone_number if !phone_number.empty?
+     entry.email = email if !email.empty?
+     system "clear"
+     # #6
+     puts "Updated entry:"
+     puts entry
     end
 
     def read_csv
+
+      print "Enter CSV file to import: "
+      file_name = gets.chomp
+
+      # check if file name is empty
+      if file_name.empty?
+        system "clear"
+        puts "No CSV file read"
+        main_menu
+      end
+      #here we import the csv file and clear the system. if the file does not exsist we prompt the user to try again
+      begin
+        entry_count = address_book.import_from_csv(file_name).count
+        system "clear"
+        puts "#{entry_count} new entries added from #{file_name}"
+      rescue
+        puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+        read_csv
+      end
+
     end
 
     def entry_submenu(entry)
@@ -128,7 +183,10 @@ class MenuController
         # when the user asks to see the next entry, we can do nothing and control will be returned to veiw_all_entries
         when "n"
         when "d"
+          delete_entry(entry)
         when "e"
+          edit_entry(entry)
+          entry_submenu(entry)
         when "m"
           system "clear"
           main_menu
@@ -138,5 +196,34 @@ class MenuController
           entry_submenu(entry)
         end
       end
+
+    def search_submenu(entry)
+
+      puts "\nd - delete entry"
+      puts "e - edit this entry"
+      puts "m - return to main menu"
+
+      selection = gets.chomp
+
+
+      case selection
+        when "d"
+          system "clear"
+          delete_entry(entry)
+          main_menu
+        when "e"
+          edit_entry(entry)
+          system "clear"
+          main_menu
+        when "m"
+          system "clear"
+          main_menu
+        else
+          system "clear"
+          puts "#{selection} is not a valid input"
+          puts entry.to_s
+          search_submenu(entry)
+      end
+    end
 
 end
